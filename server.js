1cad54a16e907cdb588f2dce04cf7ea290714b7c
Ardiy0011 <ardiyquarsh@gmail.com>
2024-03-31 12:32:32 -0700

    remove session

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const session = require('express-session');
 const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
 const winston = require('winston');
@@ -46,11 +45,6 @@ db.once('open', () => {
 });
 
 // Set up express-session
-app.use(session({
-    secret: process.env.SECRET_KEY,
-    resave: false,
-    saveUninitialized: true,
-}));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -102,6 +96,5 @@ app.get('/dashboard', async (req, res) => {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    logger.info(`Server is running on port ${port}`);
     console.log(`Server is running on port ${port}`);
 });
