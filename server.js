85943ff9ff9e5b5f3ed84b4a7a3d6527566bc5cc
Ardiy0011 <ardiyquarsh@gmail.com>
2024-04-14 18:12:41 -0700

    update cors configurations for jang

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ const app = express();
 
 // Set up CORS
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3001', 'https://joseph-x-geraldine.we-devsgh.online/'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3001', 'https://joseph-x-geraldine.we-devsgh.online/Rsvp'],
     credentials: true,
 }));
-
 
 // Set up logging
 const logger = winston.createLogger({
